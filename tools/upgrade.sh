7661fd3d3462e4fd135961b701b56a1faf65bc2b
Rouzbeh Delavari <rouzwawi@gmail.com>
2013-04-06 12:20:31 +0200

    rebase on update

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -22,7 +22,8 @@ fi
 
 printf "${BLUE}%s${NORMAL}\n" "Updating Oh My Zsh"
 cd "$ZSH"
-if git pull --rebase --stat origin master
+
+if git fetch upstream && git rebase upstream/master
 then
   printf '%s' "$GREEN"
   printf '%s\n' '         __                                     __   '
